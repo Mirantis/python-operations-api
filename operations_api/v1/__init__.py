303103c1ce526f2a8ecabca59d4d8d2d5375f786
Adam Tengler <atengler@mirantis.com>
2018-10-04 20:13:25 +0200

    Move API endpoints

--- operations_api/v1/__init__.py
+++ operations_api/v1/__init__.py
@@ -6,12 +6,15 @@ from flask_restplus import Api
 from operations_api.config import settings
 from sqlalchemy.orm.exc import NoResultFound
 
-from .modelform import api as modelform
+from .modelform.endpoints import api as modelform
 
 log = logging.getLogger(__name__)
+
+# setup api BP
 blueprint = Blueprint('api', __name__)
 api = Api(blueprint)
 
+# register namespaces
 api.add_namespace(modelform)
 
 
