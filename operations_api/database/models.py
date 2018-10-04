b8d66897765a3fcfd3f953963e4880ed5c94b7be
Adam Tengler <atengler@mirantis.com>
2018-10-04 18:44:39 +0200

    Define FormInstance DB model

--- operations_api/database/models.py
+++ operations_api/database/models.py
@@ -1,5 +1,17 @@
+from flask import current_app as app
+
 from operations_api.database import db
 
+from sqlalchemy_utils import EncryptedType, UUIDType
+from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
 
-class ModelTemplate(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+SECRET_KEY = app.config['SECRET_KEY']
+
+
+class FormInstance(db.Model):
+    id = db.Column(UUIDType(binary=False),
+                   primary_key=True)
+    template = db.Column(EncryptedType(db.String,
+                                       SECRET_KEY,
+                                       AesEngine,
+                                       'pkcs5'))
