c09c2c40fe019bead182a869c09822a91ed1b0f0
Adam Tengler <atengler@mirantis.com>
2018-10-06 00:31:24 +0200

    Add modelform configuration

--- operations_api/config/settings.py
+++ operations_api/config/settings.py
@@ -2,7 +2,7 @@
 FLASK_SERVER_HOST = 'localhost'
 FLASK_SERVER_PORT = '8001'
 FLASK_DEBUG = True
-FLASK_SECRET_KEY = ''
+FLASK_SECRET_KEY = 'secretsecretsecret'
 
 # Flask-Restplus settings
 RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
@@ -14,3 +14,8 @@ RESTPLUS_ERROR_404_HELP = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 # Turn off the Flask-SQLAlchemy event system
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+try:
+    from operations_api.local_settings import *  # noqa
+except ImportError:
+    pass
