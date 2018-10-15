ea531c634ff5bb3aaea11d8ad60e143238850aa4
Ekaterina Chernova <efedorova@mirantis.com>
2018-10-15 10:38:32 +0300

    Allow CORS

--- operations_api/extensions.py
+++ operations_api/extensions.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask_oidc import OpenIDConnect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.contrib.cache import SimpleCache
@@ -5,3 +6,4 @@ from werkzeug.contrib.cache import SimpleCache
 cache = SimpleCache()
 db = SQLAlchemy()
 oidc = OpenIDConnect()
+cors = CORS()
