7911c00e73bb884622ec380d3e34f479ed00fe91
Adam Tengler <atengler@mirantis.com>
2018-10-25 22:44:14 +0200

    Fix entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if jq -e . >/dev/null 2>&1 <<< "$OAPI_OIDC_CLIENT_SECRETS_OVERRIDE"; then
+jq -e . > /dev/null 2>&1 <<< "$OAPI_OIDC_CLIENT_SECRETS_OVERRIDE"
+VALID=$?
+
+
+if [[ -n $OAPI_OIDC_CLIENT_SECRETS_OVERRIDE ]] && [ "$VALID" -eq 0 ]; then
     echo "Overriding client_secrets.json with env string into: /tmp/client_secrets.json"
     echo $OAPI_OIDC_CLIENT_SECRETS_OVERRIDE > /tmp/client_secrets.json
     export OAPI_OIDC_CLIENT_SECRETS="/tmp/client_secrets.json"
